95e61e5927804521055606c2f66ab951f3ea1fd1
ADITYA-1030 <gogoiaditya624@gmail.com>
2024-09-25 21:03:17 +0530

    Update Prime Factor.c

--- Prime Factor.c
+++ Prime Factor.c
@@ -1,17 +1,26 @@
+// C program to find the prime factors of a number 
 #include <stdio.h>
-void primefactor(int num)
-{
-	printf("Prime Factors of the Number:");
-	for(int i=2;num>1;i++){
-		while(num%1==0){
-			printf("%d",i);
-			num = num/i;
-		}
-	}
-}
-int main(){
-	int num;
-	num=12;
-	primefactor(num);
-	return 0;
+
+int main() {
+    int num, i, factor;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    printf("Prime factors of %d: ", num);
+
+    for (i = 2; i <= num; i++) {
+        while (num % i == 0) {
+            printf("%d ", i);
+            num /= i;
+        }
+    }
+
+    if (num > 1) {
+        printf("%d", num);
+    }
+
+    printf("\n");
+
+    return 0;
 }
